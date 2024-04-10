90db341fed89fc0b5b69b7d027987b49da96edbe
augustijn <av407@exeter.ac.uk>
2024-04-11 00:22:01 +0100

    remove edge

--- python code/dimensionRedwithDict.py
+++ python code/dimensionRedwithDict.py
@@ -54,9 +54,34 @@ Maintain genetic diversity:
 
 
 
+Justify choice of using correlation for probability, over combined heuristic of best edge probability and usage? test out options.
 
 
+have a stack for when generating solution -> help with add edge and remove edge specifically
+    creating graph of possible solutions impractical -> have stack which goes and goes until no possible solutions
+                                                        pop latest addition and add to impossible pile and try again
+                                                        
+                                                        reduce correlation? -> prob of popped edges? 
+                                                        depending on depth -> i.e if 10 nodes added, and 11th is impossible, pop 10th node go back to 9th 
+                                                        and try 10th node again, if all 10th's nodes are impossible for that then pop 9th and go to 8th
+                                                        9th edge which was impossible for all nodes more punished than individual 10th's nodes.
 
+                                                        test if punish all nodes tested or just the 1st one to cause an issue.
+                                                        i.e. if 2nd edge caused the problem and we only find out at the 9th edge, punish all edges tried between 2nd and 9th
+                                                        or only 2nd majorly punished?
+
+                                                        implicitly removing an edge, and then a combo edge added which forces you to use that edge isnt a great combo edge
+                                                        as it forces you to use a bad (removed) edge so should be punished 
+
+                            option 2:
+                                        just add the temporary edge: -> if fundamental edges (length 1) are removed, turn their correlation to 0, etc, and make it unable to
+                                        change -> or just add it temporarily if needed. 
+
+                                        possibly then also give a punishment to all of the edges necessary in the solution so far, allows for computationally cheap solution
+                                        enables chance of exploration and gives a negative for using a bad edge.
+
+
+                            option 2 more viable for problem where sampling isnt an issue? -> for more expensive sampling option one wins out
 
 
 
@@ -83,9 +108,25 @@ Maintain genetic diversity:
 
 """
 
+validTransition = {0:[(1,),(9,)],
+                  1:[(0,),(13,)],
+                  2:[(13,),(3,)],      
+                  3:[(2,),(4,)],
+                  4:[(5,),(3,)],
+                  5:[(4,),(11,)],
+                  6:[(11,),(12,)],
+                  7:[(12,),(10,)],
+                  8:[(10,),(9,)],
+                  9:[(0,),(8,)],
+                  10:[(8,),(7,)],
+                  11:[(5,),(6,)],
+                  12:[(7,),(6,)],
+                  13:[(2,),(1,)]} # type: ignore
+
 class vertex():
-    def __init__(self, edges, decay, offSet) -> None:
+    def __init__(self, edges, decay, offSet, ID) -> None:
         self.edges = {}
+        self.deletedReq = []
         for edge in edges:
             #key = vertexes edge travels to
             #value = [correlation, best]
@@ -94,10 +135,10 @@ class vertex():
         self.rng = np.random.default_rng()
         self.decay = decay
         self.offSet = offSet
+        self.ID = ID
         return
     
     def offsetCorrelation(self):
-
         minVal = np.inf
         for key in self.edges.keys():
             if self.edges[key][0] < minVal:
@@ -106,29 +147,48 @@ class vertex():
         if minVal < 0:
             for key in self.edges.keys():
                 self.edges[key][0] = self.decay*(self.edges[key][0] + self.offSet*(abs(minVal)))
+        elif minVal == 0:
+            minVal = 0.2
+            for key in self.edges.keys():
+                self.edges[key][0] = self.decay*(self.edges[key][0] + self.offSet*(abs(minVal)))
         else:
             for key in self.edges.keys():
                 self.edges[key][0] = self.decay*(self.edges[key][0])
         return
     
-    def getValidTransitions(self, notValid, remainingNodes):
+    def getValidTransitions(self, notValid, remainingNodes, loud=False):
         temp = notValid.copy()
-        temp.pop(0)
+        last = temp.pop(0)
         valid = []
+
         for key in self.edges.keys():
             allow = True
 
-            if len(key) == remainingNodes:
-
+            if len(key) == remainingNodes and key[-1] == last:
                 for node in key:
                     if node in temp:
                         allow = False
                         break
+
             else:
                 for node in key:
                     if node in notValid:
                         allow = False
                         break
+
+            if allow:
+                valid.append(key)
+
+        return valid
+
+    def getValidDeletedTransitions(self, notValid):
+        valid = []
+        print(self.deletedReq)
+        for key in self.deletedReq:
+            allow = True
+            for val in key:
+                if val in notValid:
+                    allow = False
             if allow:
                 valid.append(key)
 
@@ -137,8 +197,12 @@ class vertex():
     def chooseTransition(self, notValid, remainingNodes):
         valid = self.getValidTransitions(notValid, remainingNodes)
         if len(valid) == 0:
-            print("no valid found")
-            exit()
+            valid = self.getValidDeletedTransitions(notValid)
+            print("valid", valid)
+            key = np.random.random_integers(0, len(valid))
+            print(key)
+            print(valid)
+            return valid[key], True
 
         total = 0
         for key in valid:
@@ -150,22 +214,24 @@ class vertex():
         for key in valid:
             total += self.edges[key][0]
             if total > val:
-                return key
+                return key, False
         
         print("something went wrong")
         exit()
     
     def getBestTransition(self, notValid, remainingNodes):
+
         valid = self.getValidTransitions(notValid, remainingNodes)
         if len(valid) == 0:
-            print("no valid found")
-            exit()
-        
+            valid = self.getValidDeletedTransitions(notValid)
+            key = np.random.choice(valid)
+            return key
+  
         best = valid[0]
         for key in valid:
             if self.edges[key][1] < self.edges[best][1]:
                 best = key
-    
+  
         return best
 
     def updateEdgeVals(self, edgeID, score, normalisedVal) -> None:
@@ -175,19 +241,29 @@ class vertex():
             self.edges[edgeID][1] = score
         return
 
-    def addEdge(self, EdgeID, verticesToAdd):
+    def addEdge(self, EdgeID, verticesToAdd, best):
         newEdge = []
         for i in EdgeID:
             newEdge.append(i)
+   
         for i in verticesToAdd:
             newEdge.append(i)
-        newKey = tuple(newEdge)
-        self.edges[EdgeID][0] /= 2
-        self.edges[newKey] = [self.edges[EdgeID][0],self.edges[EdgeID][1]]
-        return
-    
-    def removeEdge(self):
 
+        newKey = tuple(newEdge)
+
+        if newKey in self.edges:
+            if best < self.edges[newKey][1]:
+                self.edges[newKey][1] = best
+            return
+        self.edges[newKey] = [self.edges[EdgeID][0],best]
+        return 
+    
+    def removeEdge(self, edgeID):
+        self.edges.pop(edgeID)
+        #edges of length one are needed in order to generate solutions in certain cases, add to special list to only consider
+        #in worst cases when no other valid edges exist
+        if len(edgeID) == 1:
+            self.deletedReq.append(edgeID)
         return
     
 class Graph():
@@ -221,13 +297,16 @@ class tester():
     """
 
     def __init__(self, graph:Graph, iterCount:int, popSize:int, probDecay:float, offSet:float):
+        if offSet <= 1:
+            print("offset must be greater than 1")
+            exit()
         self.verticeCount = len(graph.distanceMatrix)
         vertices = [(i,) for i in range(self.verticeCount)]
         self.vertices = np.empty(self.verticeCount, dtype=vertex)
         for i in range(len(graph.distanceMatrix)):
             temp = vertices.copy()
-            temp.pop(i)
-            self.vertices[i] = vertex(temp, probDecay, offSet)
+            id = sum(temp.pop(i))
+            self.vertices[i] = vertex(temp, probDecay, offSet, id)
 
         self.graph = graph
         self.mean = 0
@@ -240,6 +319,8 @@ class tester():
         self.ratio = 0
 
     def mainRun(self):
+        self.prevVal = np.inf
+        self.prevBest = []
         for i in range(self.iterCount):
             self.offsetProbMatrix()
             solutions = np.empty((self.popSize, 2),dtype=object)
@@ -255,9 +336,9 @@ class tester():
             for j in solutions:
                 
                 if j[0] < self.best + self.ratio*self.sd:
-                    add.append(j[1])
+                    add.append(j)
                 elif j[0] > self.mean + (2*self.sd):
-                    rm.append(j[1])
+                    rm.append(j)
 
                 normalisedVal = self.calcSolUtility(j[0])
                 self.updateCorrelation(j[1], j[0],normalisedVal)
@@ -266,11 +347,18 @@ class tester():
             
             for sol in add:
                 #solutionID format: [(vertexID, edgeID),...,...]
-                self.addEdge(sol)
-                
+                self.addEdge(sol[1], sol[0])
+            
+            for sol in rm:
+                self.removeEdge(sol[1])
+            
             self.genBestGuess()
             print(i)
         
+        print("--------------------------------\n")
+        print(self.best)
+        print(self.bestSol)
+        print("\n--------------------------------")
         self.genBestGuess()
         return
     
@@ -294,11 +382,19 @@ class tester():
         return normalisedVals
     
     def removeEdge(self, solution):
+        #vertexID, edgeID                       
         normalisedVals = self.normaliseEdges(solution)
+        if len(normalisedVals) != len(solution):
+            print("error, add edge normal values not equal to sol")
+            exit()
 
+        for i in range(len(normalisedVals)):
+            if normalisedVals[i] > 0.6:
+                self.vertices[solution[i][0]].removeEdge(solution[i][1])
+        return
 
-    def addEdge(self, solution):
-        #vertexID, edgeID
+    def addEdge(self, solution, score):
+        #vertexID, edgeID                       
         normalisedVals = self.normaliseEdges(solution)
         if len(normalisedVals) != len(solution):
             print("error, add edge normal values not equal to sol")
@@ -306,7 +402,8 @@ class tester():
         j = 0
         for i in range(1, len(normalisedVals)):
             if normalisedVals[j] < 0.3 and normalisedVals[i] < 0.3:
-                self.vertices[solution[j][0]].addEdge(solution[j][1], solution[i][1])
+                self.vertices[solution[j][0]].addEdge(solution[j][1], solution[i][1], score)
+
             j = i
         return
 
@@ -350,7 +447,7 @@ class tester():
         #solutionID format: [(vertexID, edgeID),...,...]
         for edge in solutionID:
             self.vertices[edge[0]].updateEdgeVals(edge[1], score, normalisedVal)
-    
+
     def genBestGuess(self) -> list:
         cur = np.random.randint(0, self.verticeCount)
         bestGuess = [cur]
@@ -364,12 +461,14 @@ class tester():
                 bestGuess.append(edge)
                 count += 1
                 remainingNodes -= 1
-            temp.append(edges)
+            temp.append((cur, edges))
             cur = edges[-1]
-        print(temp)
+        bestGuess.pop(0)
+        
+        print("solution format: ", temp)
         print("best guess: {} has value {} \n".format(bestGuess, self.graph.calcDistance(bestGuess)))
         print("mean: {}  sd: {} \n".format(self.mean, self.sd))
-        return
+        return bestGuess
 
     def genProbChromosome(self):
         cur = np.random.randint(0, self.verticeCount)
@@ -379,11 +478,17 @@ class tester():
         count = 0
         remainingNodes = self.verticeCount
         while count < self.verticeCount:
-            edges = self.vertices[cur].chooseTransition(solution, remainingNodes)
+            edges, isDeleted = self.vertices[cur].chooseTransition(solution, remainingNodes)
             for edge in edges:
                 solution.append(edge)
                 count += 1
                 remainingNodes -= 1
+
+            if isDeleted:
+                #dont add to solutionID, this edge was bad so shouldnt get removed again if it leads to a bad edge
+                cur = edges[-1]
+                continue
+
             solutionID.append((cur, edges))
             cur = edges[-1]
         solution.pop(0)
@@ -399,8 +504,12 @@ def fetchGraph(xmlFile:str) -> Graph:
 def main():
     
     graph = fetchGraph('burma14.xml')
+    #iter count, popsize, correlation decay, offset: >1
     agent = tester(graph, 100, 50, 0.975, 1.025)
     agent.mainRun()
+    for vertex in agent.vertices:
+        print("\n")
+        print(vertex.edges)
     # convert array into dataframe 
     """  DF = pd.DataFrame(agent.minLenMatrix) 
     DF2 = pd.DataFrame(graph.distanceMatrix)
